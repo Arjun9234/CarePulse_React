74be02f880f9577097725f6fd3916462fa093ce1
Arjun Arora <arjunarora3682@gmail.com>
2025-01-09 16:04:14 +0530

    Update route.ts

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -1,14 +1,9 @@
-import { NextResponse } from "next/server";
-
 export const dynamic = "force-dynamic";
 
 // A faulty API route to test Sentry's error monitoring
 export function GET() {
-  // Throwing an error for testing Sentry
   throw new Error("Sentry Example API Route Error");
-
-  // This line will never be reached, so it's unnecessary
-  // return NextResponse.json({ data: "Testing Sentry Error..." });
 }
+
 
 
